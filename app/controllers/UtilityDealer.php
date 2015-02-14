8b7715dd585333c479bd926a4878d2d53d2b39ae
Dilshan Amadoru <dilshanatwork@gmail.com>
2015-02-14 15:27:35 +1100

    Address information

--- app/controllers/UtilityDealer.php
+++ app/controllers/UtilityDealer.php
@@ -7,6 +7,14 @@ class UtilityDealer {
 		$dealer_address = '';
 		if (array_key_exists('address', $source)) {
 			$dealer_address = 'in ' . $source['address'];
+
+			if (array_key_exists('city', $source)) {
+				$dealer_address = $dealer_address. ', ' . $source['city'];
+
+				if (array_key_exists('state', $source)) {
+					$dealer_address = $dealer_address. ', ' . $source['state'];
+				}
+			}
 		}
 
 		return $dealer_address;
