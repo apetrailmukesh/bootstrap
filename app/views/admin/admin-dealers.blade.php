e3748ecf81e9b9c1ec39f3e75589e081cd0107e1
Dilshan Amadoru <dilshanatwork@gmail.com>
2015-02-19 23:11:27 +1100

    Dealers list

--- app/views/admin/admin-dealers.blade.php
+++ app/views/admin/admin-dealers.blade.php
@@ -6,67 +6,47 @@
 	@include('logo')
 	<div class="row">
 		<div class="small-12 medium-7 large-7 columns small-centered">
-			<ul class="tabs" data-tab>
-			  	<li class="tab-title active"><a href="#paid">Paid Dealers</a></li>
-			  	<li class="tab-title"><a href="#free">Free Dealers</a></li>
-			</ul>
-			<div class="tabs-content">
-			  	<div class="content active" id="paid">
-					<div class="row collapse">
-						<div class="small-10 medium-11 large-11 columns">
-							<table width="100%">
-								<thead>
-									<tr>
-										<th width="50%">Name</th>
-										<th width="15%">Monthly Clicks</th>
-										<th width="15%">Current Clicks</th>
-										<th width="15%">Paid Clicks</th>
-										<th width="5%">Edit</th>
-									</tr>
-								</thead>
-								<tbody>
-									@foreach($paid_dealers as $dealer)
-										<tr>
-											<td>{{ $dealer->dealer }}</td>
-											<td>{{ $dealer->monthly_clicks }}</td>
-											<td>{{ $dealer->current_clicks }}</td>
-											<td>{{ $dealer->paid_clicks }}</td>
-											<td>{{ HTML::linkRoute('get.admin.dealers.edit', 'Edit', array($dealer->id)) }}</td>
-										</tr>
-									@endforeach
-								</tbody>
-							</table>
-						</div>
-					</div>    	
-			  	</div>
-			  	<div class="content" id="free">
-					<div class="row collapse">
-						<div class="small-10 medium-11 large-11 columns">
-							<table width="100%">
-								<thead>
-									<tr>
-										<th width="50%">Name</th>
-										<th width="15%">Monthly Clicks</th>
-										<th width="15%">Current Clicks</th>
-										<th width="15%">Paid Clicks</th>
-										<th width="5%">Edit</th>
-									</tr>
-								</thead>
-								<tbody>
-									@foreach($free_dealers as $dealer)
-										<tr>
-											<td>{{ $dealer->dealer }}</td>
-											<td>{{ $dealer->monthly_clicks }}</td>
-											<td>{{ $dealer->current_clicks }}</td>
-											<td>{{ $dealer->paid_clicks }}</td>
-											<td>{{ HTML::linkRoute('get.admin.dealers.edit', 'Edit', array($dealer->id)) }}</td>
-										</tr>
-									@endforeach
-								</tbody>
-							</table>
-						</div>
-					</div>    	
-			  	</div>
+			<div class="panel callout radius">
+				<p class="tagline secondary-text">Paid Dealers</p>
+				<table width="100%">
+					<thead>
+						<tr>
+							<th width="40%" class="number-column">Name</th>
+							<th width="20%" class="number-column">Monthly Limit</th>
+							<th width="20%" class="number-column">Paid Clicks</th>
+							<th width="20%" class="number-column">Total Clicks</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($paid_dealers as $dealer)
+							<tr>
+								<td>{{ HTML::linkRoute('get.admin.dealers.edit', $dealer->dealer, array($dealer->id)) }}</td>
+								<td class="number-column">{{ $dealer->monthly_clicks }}</td>
+								<td class="number-column">{{ $dealer->current_clicks }}</td>
+								<td class="number-column">{{ $dealer->paid_clicks }}</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+			<div class="panel">
+				<p class="tagline secondary-text">Free Dealers</p>
+				<table width="100%">
+					<thead>
+						<tr>
+							<th width="70%" class="number-column">Name</th>
+							<th width="30%" class="number-column">Total Clicks</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($free_dealers as $dealer)
+							<tr>
+								<td>{{ HTML::linkRoute('get.admin.dealers.edit', $dealer->dealer, array($dealer->id)) }}</td>
+								<td class="number-column">{{ $dealer->current_clicks }}</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
