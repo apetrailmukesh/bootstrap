ce06fe7f968ee4917fdbc950ca66c7907ee0bad9
Dilshan Amadoru <dilshanatwork@gmail.com>
2015-02-27 14:32:22 +1100

    Bug fixes

--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -14,10 +14,6 @@
 	<link rel="stylesheet" href="{{ URL::asset('css/foundation.min.css') }}">
 	<link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
 	<link rel="stylesheet" href="{{ URL::asset('css/custom.css') }}">
-
-	<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-60131582-1', 'auto'); ga('send', 'pageview');
-	</script>​
 </head>
 <body class="{{ $body_class }}">
 	@yield('contents')
@@ -34,5 +30,9 @@
 	<script src="{{ URL::asset('js/pikaday.js') }}"></script>
 	<script src="{{ URL::asset('js/main.js') }}"></script>
 	<script src="{{ URL::asset('js/application.js') }}"></script>
+
+	<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-60131582-1', 'auto'); ga('send', 'pageview');
+	</script>​
 </body>
 </html>
