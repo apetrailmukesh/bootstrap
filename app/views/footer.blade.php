07860c3da264874bf094d605283fcc1517b199af
Dilshan Amadoru <dilshanatwork@gmail.com>
2015-02-27 11:56:31 +1100

    Fixes

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
 	<div class="row">
 		<div class="small-12 small-centered text-center column">
-			<span>&copy; 2015 SiteName.com</span>
+			<span>&copy; 2015 carfetch.com</span>
 			<span>
 				<a href="#">Terms of Use</a><a href="#">Privacy Policy</a><a href="#">Site Map</a>
 			</span>
