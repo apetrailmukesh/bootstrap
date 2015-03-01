66bf0c9fd187c8227a8a0e415d0c4190510c5b6a
Dilshan Amadoru <dilshanatwork@gmail.com>
2015-03-02 10:11:39 +1100

    Styles fix

--- app/views/home/home-search.blade.php
+++ app/views/home/home-search.blade.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="small-12 medium-7 large-7 columns small-centered">
 			<div class="row collapse">
-				<div class="small-10 medium-11 large-11 columns">
+				<div class="small-10 medium-11 large-11 columns noSwipe">
 					{{ Form::text('search_text', Input::old('search_text'),  array('placeholder'=>'Enter make, model, or style', 'class' => 'vehicle-search')) }}
 				</div>
 				<div class="small-2 medium-1 large-1 columns">
