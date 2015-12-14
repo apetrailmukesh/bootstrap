9dfeac62f080f360ecec6dce43e9247f7bf83ede
RajKumarBhardwaj <apetrail.raj@gmail.com>
2015-12-14 17:03:37 +0530

    Signed-off-by: RajKumarBhardwaj <apetrail.raj@gmail.com>

--- mywork/Carfetch (1)/Carfetch/Code/automotive-search-engine/app/views/footer.blade.php
+++ mywork/Carfetch (1)/Carfetch/Code/automotive-search-engine/app/views/footer.blade.php
@@ -1,1 +1,10 @@
-sdfghj
+<footer>
+	<div class="row">
+		<div class="small-12 small-centered text-center column">
+			<span>&copy; 2015 carfetch.com</span>
+			<span>
+				<a href="#">Terms of Use</a><a href="#">Privacy Policy</a><a href="#">Site Map</a>
+			</span>
+		</div>
+	</div>
+</footer>
